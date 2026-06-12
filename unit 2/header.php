3553c24e7a395c50455ec5eb52075b8d1000a5ec
RajPopat-hash <rajraghuvanshi7706@gmail.com>
2026-06-12 08:57:03 +0530

    Add files via upload

---
+++ unit 2/header.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<style>
+    h1{
+        text-align: center;
+        font-size: 20px;
+        color: white;
+        background-color: black;
+    }
+</style>
+<body>
+    <br><br><br>
+    <header>
+    <h1>Welcome to My Program</h1>
+</header>
+</body>
+</html>
